667878be45d567a66d40f7140e6c31065bd5dcf4
senghonghour <39426216+senghonghour@users.noreply.github.com>
2018-07-29 00:10:42 +0700

    slight change to json

---
+++ js/about.js
@@ -0,0 +1,12 @@
+var data = [
+    {
+        "who_are_we" : "WHO ARE WE?"
+    }
+];
+
+
+for (var i = 0; i < data.length; i++) {
+    var obj = data[i];
+    
+    console.log("Name: " + obj.who_are_we);
+}
