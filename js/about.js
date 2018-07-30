98cf53c521dcad357ab6cdb69b7d47b4e6fdca72
senghonghour <39426216+senghonghour@users.noreply.github.com>
2018-07-30 16:24:38 +0700

    simplified js

--- js/about.js
+++ js/about.js
@@ -1,35 +1,28 @@
 var defaultLangEn = true;
-var navEn = document.getElementById("nav-desktop-en");
-var navKh = document.getElementById("nav-desktop-kh");
-var navTabEn = document.getElementById("nav-tab-en");
-var navTabKh = document.getElementById("nav-tab-kh");
-var pageTitleEn = document.getElementById("page-title-en");
-var pageTitleKh = document.getElementById("page-title-kh");
-var accordionEn = document.getElementById("accordion-en");
-var accordionKh = document.getElementById("accordion-kh");
-
+var classEn = document.querySelectorAll("h2.english, ul.english, div.english");
+var classKh = document.querySelectorAll("h2.khmer, ul.khmer, div.khmer");
 
 function langChange() {
     var currentLang = defaultLangEn;
     if (currentLang == true) {
-        navEn.classList.add("uk-hidden");
-        navKh.classList.remove("uk-hidden");
-        navTabEn.classList.add("uk-hidden");
-        navTabKh.classList.remove("uk-hidden");
-        pageTitleEn.classList.add("uk-hidden");
-        pageTitleKh.classList.remove("uk-hidden");
-        accordionEn.classList.add("uk-hidden");
-        accordionKh.classList.remove("uk-hidden");
+        
+        for (var i = 0; i < classEn.length; ++i) {
+            classEn[i].classList.add("uk-hidden");
+        };
+        for (var i = 0; i < classKh.length; ++i) {
+            classKh[i].classList.remove("uk-hidden");
+        };
+        
         return defaultLangEn = false;
     } else {
-        navEn.classList.remove("uk-hidden");
-        navKh.classList.add("uk-hidden");
-        navTabEn.classList.remove("uk-hidden");
-        navTabKh.classList.add("uk-hidden");
-        pageTitleEn.classList.remove("uk-hidden");
-        pageTitleKh.classList.add("uk-hidden");
-        accordionEn.classList.remove("uk-hidden");
-        accordionKh.classList.add("uk-hidden");
+        
+        for (var i = 0; i < classEn.length; ++i) {
+            classEn[i].classList.remove("uk-hidden");
+        };
+        for (var i = 0; i < classKh.length; ++i) {
+            classKh[i].classList.add("uk-hidden");
+        };
+        
         return defaultLangEn = true;
     }
 }
