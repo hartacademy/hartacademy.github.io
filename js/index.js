c91ae3f0b44a80f3506429b050dc0181ee35f201
senghonghour <39426216+senghonghour@users.noreply.github.com>
2018-08-07 14:20:31 +0700

    fixed card link issue

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@ var defaultLangEn = true;
 
 var classEn = document.querySelectorAll("ul.english, div.english, p.english");
 var classKh = document.querySelectorAll("ul.khmer, div.khmer, p.khmer");
+var cardAbout = document.getElementById("card-about");
+var cardSchedule = document.getElementById("card-schedule");
+var cardOrkas = document.getElementById("card-orkas");
+var cardContact = document.getElementById("card-contact");
+var cardArtist = document.getElementById("card-artist");
+
 function langChange() {
     var currentLang = defaultLangEn;
     if (currentLang == true) {
@@ -13,6 +19,12 @@ function langChange() {
             classKh[i].classList.remove("uk-hidden");
         };
         
+        cardAbout.href="about.html#langkh";
+        cardSchedule.href="schedule.html#langkh";
+        cardOrkas.href="orkas.html#langkh";
+        cardContact.href="contact.html#langkh";
+        cardArtist.href="artist.html#langkh";
+        
         return defaultLangEn = false;
     } else {
         
@@ -23,6 +35,12 @@ function langChange() {
             classKh[i].classList.add("uk-hidden");
         };
         
+        cardAbout.href="about.html";
+        cardSchedule.href="schedule.html";
+        cardOrkas.href="orkas.html";
+        cardContact.href="contact.html";
+        cardArtist.href="artist.html";
+        
         return defaultLangEn = true;
-    }
+    };
 }
