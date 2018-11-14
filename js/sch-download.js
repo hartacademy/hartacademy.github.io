9d2a26a262d6389eddc5de661a04dbf1de603426
senghonghour <39426216+senghonghour@users.noreply.github.com>
2018-11-14 17:15:16 +0700

    switched schedule saver to html2canvas

--- js/sch-download.js
+++ js/sch-download.js
@@ -1,11 +1,8 @@
 var schDownBtn = document.getElementById("download-schedule-btn");
-var schContent = document.getElementById("schedule-content");
+var schContent = document.querySelector("#schedule-content");
 
 schDownBtn.onclick = function() {
-    domtoimage.toPng(schContent)
-    .then(function(dataUrl) {
-        var img = new Image();
-        img.src = dataUrl;
-        window.saveAs(dataUrl, 'H-Art-schedule.png');
+    html2canvas(schContent).then(canvas => {
+        saveAs(canvas.toDataURL(), 'h-art-schedule.png');
     });
 }
