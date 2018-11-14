09b414eb397a25b8d1b0ea79377f0ecbe60745fa
senghonghour <39426216+senghonghour@users.noreply.github.com>
2018-11-14 16:48:07 +0700

    new save file method

--- js/sch-download.js
+++ js/sch-download.js
@@ -2,8 +2,10 @@ var schDownBtn = document.getElementById("download-schedule-btn");
 var schContent = document.getElementById("schedule-content");
 
 schDownBtn.onclick = function() {
-    domtoimage.toBlob(document.getElementById("schedule-content"))
-    .then(function(blob) {
-        window.saveAs(blob, 'H-Art-schedule.jpg');
+    domtoimage.toPng(schContent)
+    .then(function(dataUrl) {
+        var img = new Image();
+        img.src = dataUrl;
+        window.saveAs(dataUrl, 'H-Art-schedule.png');
     });
 }
